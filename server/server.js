e6d47b5e04815b4f6929b47e56056aa2a6623af2
Brandon Soto <brandon.soto09@gmail.com>
2018-05-24 20:42:21 -0700

    Add GET /todos

--- server/server.js
+++ server/server.js
@@ -9,12 +9,22 @@ var app = express();
 app.use(bodyparser.json());
 
 app.post('/todos', (req, res) => {
-    console.log(req.body);
+    console.log('POST /todos called');
 
     var todo = new Todo({text: req.body.text});
 
     todo.save().then((doc) => {
         res.send(doc);
+    }, (err) => {
+        res.status(400).send(err);
+    });
+});
+
+app.get('/todos', (req, res) => {
+    console.log('GET /todos called');
+
+    Todo.find().then((todos) => {
+        res.send({todos});
     }, (err) => {
         res.status(400).send(err);
     });
